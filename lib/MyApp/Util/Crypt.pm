47604e25ed1ec8a14364d8b66b3ace6720aa255d
John Doe <johndoe@example.com>
2026-04-04 17:27:23 +0200

    Check-in

--- lib/MyApp/Util/Crypt.pm
+++ lib/MyApp/Util/Crypt.pm
@@ -4,18 +4,39 @@ use strict;
 use utf8;
 use warnings;
 
+BEGIN { $^W = 0; }
+
+use Sodium::FFI qw(
+  crypto_aead_chacha20poly1305_ietf_encrypt
+  crypto_aead_chacha20poly1305_ietf_decrypt
+  crypto_aead_chacha20poly1305_IETF_NPUBBYTES
+);
+
+use MIME::Base64 qw(encode_base64 decode_base64);
+use Encode qw(decode encode);
+
 sub encrypt {
 	my $self = shift;
 
 	my %args = @_;
 
-	my $in = $args{in};
+	my %res = ();
 
-	my %res = (
-		message => "Input was $in",
-		out     => "tbd$$",
-		aha     => "604041xy\-$in"
-	);
+	my $plain = $args{plain};
+        my $key   = $args{key} // '0123456789abcdef0123456789abcdef';
+
+        my $nonce = pack("H*", '000000000000000000000000'); # -- 12 Byte (96 bit)
+        my $ad    = undef; # -- Associated Data (optional)
+
+	eval {
+		$res{encrypted}        = crypto_aead_chacha20poly1305_ietf_encrypt($plain, $ad, $nonce, $key);
+		$res{encrypted_base64} = encode_base64($res{encrypted}, '');
+	} or
+	do {};
+
+	$res{message} = "Plain input was $plain";
+	$res{out}     = "tbd$$";
+	$res{aha}     = "604041xy\-$plain";
 
 	return \%res;
 }
