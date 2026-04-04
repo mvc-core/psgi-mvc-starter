daa1b34dab4c9f2249450fb11ad700c2b8f19111
John Doe <johndoe@example.com>
2026-04-04 10:04:53 +0200

    Check-in

--- lib/MyApp/Controller/Home.pm
+++ lib/MyApp/Controller/Home.pm
@@ -2,15 +2,19 @@ package MyApp::Controller::Home;
 
 use strict;
 use warnings;
+
 use MyApp::DB;
 
 sub index {
+    my ($params, $env) = @_;
+
     my $dbh = MyApp::DB::get_dbh();
 
     my ($value) = $dbh->selectrow_array("SELECT firstname FROM users_addr LIMIT 1");
 
     return {
-        name => $value,
+        name => 'iii'.$value,
+	env  => $env
     };
 }
 
