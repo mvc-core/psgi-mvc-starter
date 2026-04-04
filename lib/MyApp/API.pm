e26640be15487fc905634b10fd987ba83982d26e
John Doe <johndoe@example.com>
2026-04-04 18:55:03 +0200

    Check-in

--- lib/MyApp/API.pm
+++ lib/MyApp/API.pm
@@ -1,5 +1,7 @@
 package MyApp::API;
+
 use Dancer2;
+use MyApp::DB;
 
 set serializer => 'JSON';
 
@@ -43,10 +45,13 @@ post '/secure' => sub {
     my $password = $data->{password};
     my $tenant   = $data->{tenant} // '* No tenant *';
 
+    my $dbh     = MyApp::DB::get_dbh();
+    my ($value) = $dbh->selectrow_array("SELECT firstname FROM users_addr LIMIT 1");
+
     return {
         ok          => 1,
         api_version => 0.8002,
-        msg         => "Username was $user, PID $$, Req from tenant $tenant, resp from $ENV{HOSTNAME}"
+        msg         => "Username was $user / DB: $value, PID $$, Req from tenant $tenant, resp from $ENV{HOSTNAME}"
     };
 };
 
