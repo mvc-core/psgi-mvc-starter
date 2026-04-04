3996defe7742e93c39771c6673cd2242ca238854
John Doe <johndoe@example.com>
2026-04-04 16:26:52 +0200

    Check-in

---
+++ lib/MyApp/Service/Auth.pm
@@ -0,0 +1,24 @@
+package MyApp::Service::Auth;
+
+use strict;
+use utf8;
+use warnings;
+# XX use DBI;
+
+my $dbh;
+my $dbh_pid;
+
+# XXX my %CONNECT_ARGS = (
+# XXX );
+
+sub _test {
+	my %foo = (
+		aha => '604041xy'
+	);
+	return \%foo;
+}
+
+# sub get_dbh {
+# }
+
+1;
