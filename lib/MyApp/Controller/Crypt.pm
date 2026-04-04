8ab0e292551c5d6a65ae921362d8276731d11b39
John Doe <johndoe@example.com>
2026-04-04 16:54:48 +0200

    Further Development (ALGONETIC)

--- lib/MyApp/Controller/Crypt.pm
+++ lib/MyApp/Controller/Crypt.pm
@@ -5,28 +5,24 @@ use warnings;
 use utf8;
 
 # use MyApp::DB;
-use LWP::UserAgent;
-use HTTP::Request;
-use JSON qw(encode_json decode_json);
-# XX use MyApp::Service::Auth;
+# XX use LWP::UserAgent;
+# XX use HTTP::Request;
+# XX use JSON qw(encode_json decode_json);
 
 sub index {
     my ($params, $env) = @_;
 
     my $session = $env->{'psgix.session'};
-    # XXX my $dbh     = MyApp::DB::get_dbh();
-    # XXX my ($value) = $dbh->selectrow_array("SELECT firstname FROM users_addr LIMIT 1");
 
     my $result;
-    # XX my $token = 'secret';
-    $result->{out} = 'asdfxx';
+
+    $result->{out} = "asdfxx$$";
 
     return {
         name    => 'Controller::Crypt',
 	env     => $env,
-	# XXX cookies => $cookies,
 	result  => $result,
-	xy      => "Frisch v/<b>Controller</b> lib/MyApp/Controller/Login. $params->{user}"
+	xy      => "Frisch v/<b>Controller</b> lib/MyApp/Controller/Crypt. $params->{in}"
     };
 }
 
