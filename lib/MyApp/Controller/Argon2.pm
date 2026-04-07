c1fb1bf85d56101facd2030d899252a67bf1b74f
John Doe <johndoe@example.com>
2026-04-07 18:26:41 +0200

    Check-in

--- lib/MyApp/Controller/Argon2.pm
+++ lib/MyApp/Controller/Argon2.pm
@@ -10,6 +10,9 @@ use HTTP::Request;
 use JSON qw(encode_json decode_json);
 use MyApp::Service::Auth;
 
+use Crypt::Argon2 qw/argon2id_pass argon2id_verify/;
+use Bytes::Random::Secure qw(random_bytes random_bytes_base64 random_bytes_hex);
+
 sub index {
     my ($params, $env) = @_;
 
@@ -18,12 +21,23 @@ sub index {
     my ($value) = $dbh->selectrow_array("SELECT firstname FROM users_addr LIMIT 1");
 
     my $result;
+    my @msgs = ();
     my $token = 'secret';
     
     my $api_endpoint = 'https://psgi.h3.zspace.ch/api/secure';
        $api_endpoint = 'http://localhost/api/secure' if $env->{HTTP_HOST} eq 'psgi.h3.zspace.ch' or 1==1;
 
     (my $user = $params->{user}) =~ tr/ //d;
+
+    my $password_hash = 'XXX';
+
+    my $granted = my $f_argon_error = 0;
+    eval { $granted = argon2id_verify($password_hash, $params->{pass}); };
+    if ($@) {
+    	my $errstr = "Error $@";
+        push(@msgs, "E510252: $errstr");
+        $f_argon_error = 1;
+    }
 
     if ( $user ) {
         my $ua  = LWP::UserAgent->new;
