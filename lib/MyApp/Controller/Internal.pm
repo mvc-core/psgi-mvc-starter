b77695cf56d7957867c83fa3634d8fa3b4718338
John Doe <johndoe@example.com>
2026-04-24 17:32:05 +0200

    Check-in

--- lib/MyApp/Controller/Internal.pm
+++ lib/MyApp/Controller/Internal.pm
@@ -9,7 +9,7 @@ use MyApp::Service::Auth;
 use Crypt::Lite;
 use ScreenPoint::Prism;
 
-my $crypt = Crypt::Lite->new( encoding => 'hex8 ');
+# XX my $crypt = Crypt::Lite->new( encoding => 'hex8 ');
 
 sub index {
     my ($params, $env) = @_;
@@ -46,7 +46,7 @@ sub index {
         env         => $env,
 	is_logged_in => MyApp::Service::Auth::_is_logged_in($env),
         cookies     => $cookies,
-        xy          => "hhhDie PID ist jetzt gerade $$ / " . $crypt->encrypt('foo', 'bar'),
+        xy          => "eee Die PID ist jetzt gerade $$ / ",
         _set_cookies => \@set_cookies,
     };
 }
